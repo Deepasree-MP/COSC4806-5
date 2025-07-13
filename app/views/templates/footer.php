57cd4c10acb9b4e7affd4c3862c74ac0464b81aa
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-07-13 20:36:35 +0000

    Add text in footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -2,13 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-left">
-                <p>Copyright &copy; <?= date('Y'); ?></p>
+                <p>&copy; <?= date('Y'); ?> COSC 4806 App</p>
             </div>
         </div>
     </div>
 </footer>
-
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
