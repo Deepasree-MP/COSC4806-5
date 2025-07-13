d5e230eaf0965d8797261dd4523bb78253a4cfec
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-07-13 20:42:32 +0000

    footer text update

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -2,11 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-left">
-                <p>&copy; <?= date('Y'); ?> COSC 4806 App</p>
+                <p class="mb-1">COSC 4806 Assignment App - Copyright &copy; <?= date('Y'); ?></p>
             </div>
         </div>
     </div>
 </footer>
+
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
